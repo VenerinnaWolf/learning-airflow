00d5ee4439b2da532b589fb93ef7cf11fe12db92
Veronika <101918389+VenerinnaWolf@users.noreply.github.com>
2025-04-25 13:59:22 +0300

    Added spaces

--- python_tasks/first/correlations.py
+++ python_tasks/first/correlations.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
+
 def get_highest_correlation(df: pd.DataFrame) -> tuple:
-    """Возвращает имена двух столбцов с наибольшей положительной корреляцией для датафрейма"""
+    """Возвращает имена двух столбцов c наибольшей положительной корреляцией для датафрейма"""
     # Вычисляем корреляционную матрицу по методу Пирсона:
     corr_mat = df.corr()
 
     # Запишем все корреляции из нижнего треугольника матрицы (с названиями строк и столбцов)
     corrs = [
-        (corr_mat.iloc[row, col], corr_mat.columns[row], corr_mat.columns[col]) 
+        (corr_mat.iloc[row, col], corr_mat.columns[row], corr_mat.columns[col])
         for row in range(corr_mat.shape[0]) for col in range(row)  # нижний треугольник
     ]
 
     # Найдем наибольший элемент из полученных корреляций и возвращаем только названия столбцов:
     return max(corrs, key=lambda x: x[0])[1:]
+
 
 if __name__ == "__main__":
     data = {
